2cf33d6e128406c2c14ecbd13ebb9d99d8bd1b96
Nish1ry0 <r-nishiuchi@seassist.co.jp>
2025-04-26 00:14:29 +0900

    Inconsistent merge state: could not find file MERGE_MSG in .git folder. This file contains the commit message for a merge commit.

--- src/method/q09/Even.java
+++ src/method/q09/Even.java
@@ -1,18 +1,25 @@
 package method.q09;
 
 public class Even {
-	public static void main(String[] args) {
-		checkEven(10);
-		checkEven(5);
+	public boolean checkEven(int num) {
+		return num % 2 == 0;
 	}
 
-	public static boolean checkEven(int num) {
-		if (num % 2 == 0) {
-			System.out.println(num + "は偶数です。");
-			return true;
+	public static void main(String[] args) {
+		Even evenChecker = new Even();
+
+		int num1 = 10;
+		if (evenChecker.checkEven(num1)) {
+			System.out.println(num1 + "は偶数です。");
 		} else {
-			System.out.println(num + "は奇数です。");
-			return false;
+			System.out.println(num1 + "は奇数です。");
+		}
+
+		int num2 = 5;
+		if (evenChecker.checkEven(num2)) {
+			System.out.println(num2 + "は偶数です。");
+		} else {
+			System.out.println(num2 + "は奇数です。");
 		}
 	}
 }
